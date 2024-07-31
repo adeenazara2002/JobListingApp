700f6aabf001985200f848b2855a74e60f53b512
adeenazara2002 <adinazara2001@gmail.com>
2024-07-31 20:40:19 +0500

    Header

--- lib/Screens/regScreen.dart
+++ lib/Screens/regScreen.dart
@@ -10,6 +10,86 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Color.fromRGBO(25, 23, 32, 1),
+        child: Scrollbar(
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(padding: EdgeInsets.only(left: 25)),
+                  Column(
+                    children: [
+                      // Spacer(),
+                      Padding(padding: EdgeInsets.only(top: 110)),
+                      SizedBox(
+                        width: 120,
+                      ),
+                      Text(
+                        'Let\'s sign you up',
+                        style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontFamily: 'Poppins',
+                            fontSize: 35,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              // second column
+
+              // Spacer(),
+              SizedBox(height: 10,),
+              Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 27)),
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontFamily: 'Poppins-Regular',
+                            fontSize: 30 , fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              
+               // Third column
+
+              // SizedBox(height: 10,),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 27)),
+                      Text(
+                        'Join the community!',
+                        style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontFamily: 'Poppins-Regular',
+                            fontSize: 30 , fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class JobListing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Register(),
     );
   }
